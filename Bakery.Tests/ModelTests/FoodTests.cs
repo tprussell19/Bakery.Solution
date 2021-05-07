14b88caa01091c6f4a1e43c9cd19bb9d42623de3
Thomas Russell <t.p.russell19@gmail.com>
2021-05-07 09:32:08 -0700

    add test to get breadcount with passing code in constructor

--- Bakery.Tests/ModelTests/FoodTests.cs
+++ Bakery.Tests/ModelTests/FoodTests.cs
@@ -4,13 +4,27 @@ using Bakery.Models;
 namespace Bakery.TestTools
 {
   [TestClass]
-  public class FoodTests
+  public class BreadOrderTests
   {
     [TestMethod]
     public void BreadOrderConstructor_CreateInstanceOfBreadOrder_BreadOrder()
     {
       BreadOrder newBreadOrder = new BreadOrder("1");
       Assert.AreEqual(typeof(BreadOrder), newBreadOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetBreadCount_ReturnsBreadCount_Int()
+    {
+      //Arrange
+      string orderAmount = "1";
+      BreadOrder newBreadOrder = new BreadOrder(orderAmount);
+
+      //Act
+      int result = newBreadOrder.BreadCount;
+
+      //Assert
+      Assert.AreEqual(1, result);
     }
   }
 }
