7d8331a862b5c5e932c1ca499fe952f057194edf
Thomas Russell <t.p.russell19@gmail.com>
2021-05-07 13:56:57 -0700

    add passing code for pastry constructor

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,6 +2,13 @@ namespace Bakery
 {
   public class PastryOrder
   {
+    public int PastryCount { get; set; }
+    public int PastryTotalCost { get; set; }
 
+    public PastryOrder(string userInput)
+    {
+      PastryCount = int.Parse(userInput);
+      PastryTotalCost = 0;
+    }
   }
 }
