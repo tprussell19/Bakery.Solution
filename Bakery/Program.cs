3dfb19520566265d41a26353cd89d6dc4a24d5e4
Thomas Russell <t.p.russell19@gmail.com>
2021-05-07 17:35:51 -0700

    WIP: implement UI to buy bread and pastries with price and amount bugs

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -11,6 +11,90 @@ namespace Bakery.Solutions
       Console.WriteLine("We are currently running a special on our fresh breads and pastries!");
       Console.WriteLine("Bread: $5 each | Buy 2, get 1 FREE!");
       Console.WriteLine("Pastries: $2 each | 3 for $5!");
+      BuyStuff();
+    }
+
+    static void BuyStuff()
+    {
+      Console.WriteLine("Would you like to purchase anything? (Y/N)");
+      string buyStuff = (Console.ReadLine()).ToLower();
+      if (buyStuff == "y")
+      {
+        BuyBread();
+        BuyPastry();
+      }
+      else if (buyStuff == "n")
+      {
+        Console.WriteLine("We hope to see you again soon!");
+      }
+      else
+      {
+        Console.WriteLine("Please enter 'Y' or 'N' only");
+        BuyStuff();
+      }
+    }
+
+    static void BuyBread()
+    {
+      Console.WriteLine("Would you like to buy some bread? (Y/N)");
+      string buyBread = (Console.ReadLine()).ToLower();
+      if (buyBread == "y")
+      {
+        Console.WriteLine("How many loaves of bread would you like?");
+        string userInput = Console.ReadLine();
+        int parsedInput = int.Parse(userInput);
+        if (parsedInput <= 0)
+        {
+          Console.WriteLine("Please enter a valid number.");
+          BuyBread();
+        }
+        else
+        {
+          BreadOrder newBreadOrder = new BreadOrder(userInput);
+          Console.WriteLine("That will be $" + newBreadOrder.BreadTotalCost + " for " + " loaves of bread!");
+          BuyPastry();
+        }
+      }
+      else if (buyBread == "n")
+      {
+        BuyPastry();
+      }
+      else
+      {
+        Console.WriteLine("Please enter 'Y' or 'N' only");
+        BuyBread();
+      }
+    }
+
+    static void BuyPastry()
+    {
+      Console.WriteLine("Would you like to buy some pastries? (Y/N)");
+      string buyPastry = (Console.ReadLine()).ToLower();
+      if (buyPastry == "y")
+      {
+        Console.WriteLine("How many pastries would you like?");
+        string userInput = Console.ReadLine();
+        int parsedInput = int.Parse(userInput);
+        if (parsedInput <= 0)
+        {
+          Console.WriteLine("Please enter a valid number.");
+          BuyPastry();
+        }
+        else
+        {
+          PastryOrder newPastryOrder = new PastryOrder(userInput);
+          Console.WriteLine("That will be $" + newPastryOrder.PastryTotalCost + " for " + " pastries!");
+        }
+      }
+      else if (buyPastry == "n")
+      {
+        Console.WriteLine("Thank you for choosing Pierre's Bakery! We hope to see you again soon!");
+      }
+      else
+      {
+        Console.WriteLine("Please enter 'Y' or 'N' only");
+        BuyPastry();
+      }
     }
   }
 }
